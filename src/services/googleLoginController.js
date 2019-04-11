0953b40a8752cb833086e59f3acffc9779cd90d0
queerPassenger <queerpassenger@gmail.com>
2019-04-11 12:26:20 +0530

    ValidateCreateUserLoginId&UserIdCheck

--- src/services/googleLoginController.js
+++ src/services/googleLoginController.js
@@ -4,23 +4,36 @@ const { errorConstants } = require('../constants');
 const dbCollectionData=dbCollectionSet[0];
 const dbName=dbCollectionData.db;
 const dbDetailsToConnect = getDbData(dbName);
+const ObjectID = require('mongodb').ObjectID;
 
 class GoogleLoginController{    
     /**
-     * @param {Object} profile
+     * @param {Object} queryObj
+     * @param {boolean} createFlag
      * @return {Promise}
      */
-    static checkUserExist(profile,createFlag){
-        return new Promise((resolve,reject)=>{           
+    static checkUserExist(queryObj,createFlag){
+        return new Promise((resolve,reject)=>{   
+            // {userId:queryObj.id} For all req
+            // {loginId:queryObj.id} For Create req
+            let query;
+            if(createFlag){
+                query={loginId:queryObj.id};
+            }
+            else{
+                query={userId:queryObj.id};
+            }
+            console.log('query',query)
             dbConnect(dbDetailsToConnect, (dbClient) => {
-                dbClient.db(dbDetailsToConnect.db_name).collection('user').findOne({loginId:profile.id},{projection:{userId:1,_id:0}},(err, result)=>{
+                dbClient.db(dbDetailsToConnect.db_name).collection('user').findOne(query,{projection:{userId:1,_id:0}},(err, result)=>{
                    if(err){
                         reject(errorConstants.findUserFailedMsg);
                     }
                     else{
+                        console.log('result',result);   
                         if(result===null){
                             if(createFlag){
-                                const createNewUserHandler=GoogleLoginController.createNewUser(profile); 
+                                const createNewUserHandler=GoogleLoginController.createNewUser(queryObj); 
                                 createNewUserHandler
                                 .then((status)=>{
                                     resolve(status);
@@ -33,8 +46,13 @@ class GoogleLoginController{
                                 reject(errorConstants.userNotFoundMsg)
                             }
                         }
-                        else
-                            resolve('User already exists');
+                        else{
+                            if(createFlag)
+                                reject('User already exists')
+                            else
+                                resolve('User exist');
+                        }
+                            
                     }
                 })
             })
@@ -47,8 +65,10 @@ class GoogleLoginController{
                 let schemaString =JSON.stringify(skeletonSchema(dbName,'user').skeletonSchema);
                 dbCollectionData.dataTypeSkeleton.map((dataType) => {
                     schemaString = schemaString.replace(new RegExp(dataType, 'g'), dummyEquivalent(dataType));
-                })
-                let userSchema=JSON.parse(schemaString);
+                });
+                let objId=new ObjectID();
+                let userSchema=JSON.parse(schemaString);                
+                userSchema.userId=objId.toString();
                 userSchema.loginType=profile.provider;
                 userSchema.loginId=profile.id;
                 userSchema.userName=profile.displayName;
@@ -60,11 +80,10 @@ class GoogleLoginController{
                         { upsert: true },
                         (err,result)=>{
                             if(err){
-                                console.log('Error is   ',err);
                                 reject(errorConstants.createNewUserFailedMsg)
                             }
                             else{
-                                resolve('User registered successfully');
+                                resolve(userSchema.userId);
                             }
                         }
                     )
