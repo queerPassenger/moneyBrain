0baa6880f7c6574c4121e0e5743f5567c47faac6
queerPassenger <queerpassenger@gmail.com>
2019-06-12 09:25:37 +0530

    Updating Scripts in Package.json

--- src/mongoUtilities/mongoConnector.js
+++ src/mongoUtilities/mongoConnector.js
@@ -5,6 +5,7 @@ const createConnection=function(dbConnectionUrl,callback){
 	console.log('**Connection Establishment Begins**',++mongoCount);
 	mongoClient.connect(dbConnectionUrl, { useNewUrlParser: true },function(err,dbInstance){
 		if(err){
+			console.log('Err',err)
 			callback(err,dbInstance);
 		}
 		else{
